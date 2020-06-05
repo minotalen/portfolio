4b9da28aaac467c9b31af4ef37716760db6de8be
mino <konformitaet@gmail.com>
2020-06-05 19:15:25 +0200

    lets try

---
+++ assets/js/8.ba05cfe4.js
@@ -0,0 +1,90 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [8],
+  {
+    420: function(s, t, a) {
+      "use strict";
+      a.r(t);
+      var i = a(25),
+        d = Object(i.a)(
+          {},
+          function() {
+            var s = this,
+              t = s.$createElement,
+              a = s._self._c || t;
+            return a(
+              "ContentSlotsDistributor",
+              { attrs: { "slot-key": s.$parent.slotKey } },
+              [
+                a("div", { attrs: { id: "schaltkreis" } }, [
+                  a("div", { staticClass: "wordBox bin" }, [
+                    a("div", { staticClass: "schaltWord bin" }, [s._v("bin")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox blind" }, [
+                    a("div", { staticClass: "schaltWord blind" }, [
+                      s._v("blind")
+                    ])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox dann" }, [
+                    a("div", { staticClass: "schaltWord dann" }, [s._v("dann")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox wenn" }, [
+                    a("div", { staticClass: "schaltWord wenn" }, [s._v("wenn")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox ich" }, [
+                    a("div", { staticClass: "schaltWord ichCenter" }, [
+                      s._v("ich")
+                    ])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox nicht" }, [
+                    a("div", { staticClass: "schaltWord nicht" }, [
+                      s._v("nicht")
+                    ])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox dass" }, [
+                    a("div", { staticClass: "schaltWord dass" }, [s._v("dass")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox sehe" }, [
+                    a("div", { staticClass: "schaltWord sehe" }, [s._v("sehe")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "wordBox aber" }, [
+                    a("div", { staticClass: "schaltWord aber" }, [s._v("aber")])
+                  ]),
+                  s._v(" "),
+                  a("div", { staticClass: "gap g1" }),
+                  s._v(" "),
+                  a("div", { staticClass: "gap g2" }),
+                  s._v(" "),
+                  a("div", { staticClass: "gap g3" }),
+                  s._v(" "),
+                  a("div", { staticClass: "gap g4" }),
+                  s._v(" "),
+                  a("div", { staticClass: "gapv1" }),
+                  s._v(" "),
+                  a("div", { staticClass: "gapv2" })
+                ]),
+                s._v(" "),
+                a("div", { attrs: { id: "quoteshow" } }),
+                s._v(" "),
+                a("div", { attrs: { id: "source" } }),
+                a("script", { attrs: { src: "schaltkreis.js" } })
+              ]
+            );
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        );
+      t.default = d.exports;
+    }
+  }
+]);
